4e8c6b616e04351a3f613c0b8d8db9132ad34a6b
scg scg <scg scg>
2016-12-15 00:11:04 +0100

    añadir pais actualizar

--- actualizar.php
+++ actualizar.php
@@ -6,6 +6,7 @@ $id = $_REQUEST['id'];
 $nombre = $_REQUEST['nombre'];
 $apellido1 = $_REQUEST['apellido1'];
 $apellido2 = $_REQUEST['apellido2'];
+$pais = $_REQUEST['pais'];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -14,7 +15,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "UPDATE usuarios SET nombre='$nombre' , apellido1='$apellido1' , apellido2='$apellido2' WHERE id='$id'";
+$sql = "UPDATE usuarios SET nombre='$nombre' , apellido1='$apellido1' , apellido2='$apellido2' , id_pais='$pais' WHERE id='$id'";
 $conn->query($sql);
 
 header('Location: tabla.php');
