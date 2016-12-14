fa3496d676486d6dac642ddee3d3a9d466fbd4ba
scg scg <scg scg>
2016-12-14 23:49:40 +0100

    minnor fix

--- editar.php
+++ editar.php
@@ -30,17 +30,17 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM usuarios WHERE id='$id'";
 
 $result = $conn->query($sql);
-$row = $result->fetch_assoc();
+$user = $result->fetch_assoc();
 
 ?>
 <form action="actualizar.php">
     <label for="nombre">Nombre </label>
-    <input type="text" name="nombre" value="<?=$row['nombre']?>">
+    <input type="text" name="nombre" value="<?=$user['nombre']?>">
     <label for="apellido1">Primer Apellido </label> 
-    <input type="text" name="apellido1" value="<?=$row['apellido1']?>">
+    <input type="text" name="apellido1" value="<?=$user['apellido1']?>">
     <label for="apellido2">Segundo Apellido </label>
-    <input type="text" name="apellido2" value="<?=$row['apellido2']?>">
-    <input type="hidden" name="id" value="<?=$row['id']?>">
+    <input type="text" name="apellido2" value="<?=$user['apellido2']?>">
+    <input type="hidden" name="id" value="<?=$user['id']?>">
     <button type="submit">Editar</button>
 </form>
 
