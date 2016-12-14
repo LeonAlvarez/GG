5a1d810e5051dba345173bd2ffc29c068682606e
scg scg <scg scg>
2016-12-14 22:54:58 +0100

    agregar.php y validaciones.php

--- login.php
+++ login.php
@@ -9,7 +9,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 var_dump($_REQUEST);
+
 $email = $_REQUEST['email'];
 $clave = $_REQUEST['pass'];
 
